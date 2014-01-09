584d7a150c5d7ba9b3c7ea10a9ed417d68b299db
Adam Raouki <adam0964@gmail.com>
2014-01-09 16:30:10 -0500

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Contact::Application.configure do
+ContactApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
