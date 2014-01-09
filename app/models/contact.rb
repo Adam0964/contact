3622865b93a0eaf4af024b0f3bbd8f56c66e3fe8
Adam Raouki <adam0964@gmail.com>
2014-01-09 16:13:55 -0500

    second commit

---
+++ app/models/contact.rb
@@ -0,0 +1,16 @@
+class Contact < MailForm::Base
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :contact_method, :validate => true
+  attribute :message
+  attribute :nickname,  :captcha  => true
+
+  # Declare the e-mail headers. 
+  def headers
+    {
+      :subject => "A new Contact",
+      :to      => "adam0964@gmail.com",
+      :from    => %("#{name}" <#{email}>)
+    }
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactApp::Application.config.secret_key_base = '8fe2c6a1cf6b5db97fd0c4a84a7a0f81e02bc9512527defce6cbd0f0678e1c956913739ba25915c9cc0825f717801e445f67f711d359a1d759f9784aec63e8b1'
